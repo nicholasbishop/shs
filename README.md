e45b48a617efae02e17e580144ff0e26e068d34e
Nicholas Bishop <nicholasbishop@gmail.com>
2020-07-05 21:03:04 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# simple-http-server
+
+goals:
+
+- uses threads instead of async
+- easy to define routes
+- ease of use at the expense of speed
+- maybe json oriented?
---
+++ examples/hello.rs
@@ -0,0 +1,11 @@
+#[derive(Clone)]
+struct State {
+    name: String,
+}
+
+fn main() {
+    let routes = vec![
+        
+    ];
+    shs::serve("127.0.0.1:1234", routes, state);
+}
